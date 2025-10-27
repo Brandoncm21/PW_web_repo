b44242cb058215b08647ef65b4b08f44e6f52c0f
Brandon Chacon <142065257+Brandoncm21@users.noreply.github.com>
2025-10-27 17:57:35 -0600

    comit

--- API2/Controllers/WeatherForecastController.cs
+++ API2/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace API.Controllers
+namespace API2.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- P_web_core/Controllers/HomeController.cs
+++ P_web_core/Controllers/HomeController.cs
@@ -1,38 +1,24 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using P_web_core.Models;
+using static System.Net.WebRequestMethods;
 
 namespace P_web_core.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IConfiguration _configuration;
+        private readonly IHttpClientFactory _factory;
+        public HomeController(IConfiguration configuration, IHttpClientFactory factory)
         {
-            _logger = logger;
+            _configuration = configuration;
+            _factory = factory;
         }
+
+        #region Iniciar Sesión
+
         [HttpGet]
         public IActionResult Index()
-        {
-            return View();
-        }
-
-
-        [HttpGet]
-        public IActionResult Registro()
-        {
-            return View();
-        }
-
-        [HttpGet]
-        public IActionResult RecuperarAcceso()
-        {
-            return View();
-        }
-
-
-        public IActionResult Principal()
         {
             return View();
         }
@@ -40,11 +26,66 @@ namespace P_web_core.Controllers
         [HttpPost]
         public IActionResult Index(UsuarioModel usuario)
         {
+            using (var context = _factory.CreateClient())
+            {
+                var urlApi = _configuration["Valores:UrlAPI"] + "Home/IniciarSesion";
+                var resultado = context.PostAsJsonAsync(urlApi, usuario).Result;
 
+                if (resultado.IsSuccessStatusCode)
+                {
+                    var datosApi = resultado.Content.ReadFromJsonAsync<UsuarioModel>().Result;
+
+                    if (datosApi != null)
+                    {
+                        HttpContext.Session.SetString("NombreUsuario", datosApi.Nombre);
+                        HttpContext.Session.SetString("NombrePerfil", datosApi.NombrePerfil);
+
+                        return RedirectToAction("Principal", "Home");
+                    }
+                }
+
+                ViewBag.Mensaje = "No se ha validado la información";
+                return View();
+            }
+        }
+
+        #endregion
+
+        #region Crear Usuarios
+
+        [HttpGet]
+        public IActionResult Registro()
+        {
             return View();
         }
+
         [HttpPost]
         public IActionResult Registro(UsuarioModel usuario)
+        {
+            using (var context = _factory.CreateClient())
+            {
+                var urlApi = _configuration["Valores:UrlAPI"] + "Home/Registro";
+                var resultado = context.PostAsJsonAsync(urlApi, usuario).Result;
+
+                if (resultado.IsSuccessStatusCode)
+                {
+                    var datosApi = resultado.Content.ReadFromJsonAsync<int>().Result;
+
+                    if (datosApi > 0)
+                        return RedirectToAction("Index", "Home");
+                }
+
+                ViewBag.Mensaje = "No se ha registrado la información";
+                return View();
+            }
+        }
+
+        #endregion
+
+        #region Recuperar Acceso
+
+        [HttpGet]
+        public IActionResult RecuperarAcceso()
         {
             return View();
         }
@@ -52,7 +93,31 @@ namespace P_web_core.Controllers
         [HttpPost]
         public IActionResult RecuperarAcceso(UsuarioModel usuario)
         {
+            using (var context = _factory.CreateClient())
+            {
+                var urlApi = _configuration["Valores:UrlAPI"] + "Home/RecuperarAcceso?CorreoElectronico=" + usuario.CorreoElectronico;
+                var resultado = context.GetAsync(urlApi).Result;
+
+                if (resultado.IsSuccessStatusCode)
+                {
+                    var datosApi = resultado.Content.ReadFromJsonAsync<UsuarioModel>().Result;
+
+                    if (datosApi != null)
+                        return RedirectToAction("Index", "Home");
+                }
+
+                ViewBag.Mensaje = "No se ha recuperado el acceso";
+                return View();
+            }
+        }
+
+        #endregion
+
+        [HttpGet]
+        public IActionResult Principal()
+        {
             return View();
         }
+
     }
 }
